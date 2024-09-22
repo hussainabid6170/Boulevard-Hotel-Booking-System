17446ba1dbb7dd044133e394ab85166529da6065
SheikhAbidHussain <hussainabid6170@gmail.com>
2024-09-22 16:35:50 +0530

    intial commit

---
+++ src/main/java/com/dragon/Boulevard_Hotel/BoulevardHotelApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.Boulevard_Hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoulevardHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoulevardHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dragon/Boulevard_Hotel/BoulevardHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.Boulevard_Hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoulevardHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
